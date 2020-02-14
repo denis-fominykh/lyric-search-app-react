490545e3660e7bf60ea1c5aaa0da529679306fc1
denis-fominykh <fominykhdenis@gmail.com>
2020-02-15 00:30:54 +0200

    displaying the top tracks' data

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import { Spinner } from '../layout/Spinner';
+import { Track } from './Track';
 
 export class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          const { track_list } = value;
+          const { track_list, heading } = value;
           if (track_list === undefined || track_list === 0) {
             return <Spinner />;
           } else {
-            return <h1>Tracks loaded</h1>;
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => {
+                    return (
+                      <Track key={item.track.track_id} track={item.track} />
+                    );
+                  })}
+                </div>
+              </React.Fragment>
+            );
           }
         }}
       </Consumer>
