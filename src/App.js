3250cb16d84898f0d17ef3a00f6ed60c8cd455cd
denis-fominykh <fominykhdenis@gmail.com>
2020-02-16 22:50:20 +0200

    creating the lyrics component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
+import { Provider } from './context';
 import { Navbar } from './components/layout/Navbar';
 import { Index } from './components/layout/Index';
-import { Provider } from './context';
+import { Lyrics } from './components/tracks/Lyrics';
 
 export function App() {
   return (
@@ -15,6 +16,7 @@ export function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Lyrics extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lyrics</h1>
+      </div>
+    );
+  }
+}
